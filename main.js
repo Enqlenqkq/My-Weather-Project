a1b56634f18ceff74cb16a42d8f7cc16ed28f7ad
Taeyoon Kim <rlaxodbs5058@gmail.com>
2025-11-23 11:36:06 +0900

    Navbar 고정

--- main.js
+++ main.js
@@ -238,3 +238,13 @@ function saveHistory(city) {
 document.addEventListener("DOMContentLoaded", () => {
   renderHistory();
 });
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
